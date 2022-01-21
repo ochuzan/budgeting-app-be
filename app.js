84ad5387c04c4017a1f206be0f50d806f20de133
Oliver Chuzan <ochuzan@gmail.com>
2022-01-21 11:09:57 -0500

    BE complete

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const transactionsController = require("./controllers/transactionController");
+// const cors = require("cors"); // dont need this yet, will need this with fe
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to I Need a Budget (INAB)");
+});
+
+app.use("/transactions", transactionsController);
+
+app.get("*", (req, res) => {
+    res.status(404).json({ error: "Page not found" });
+});
+
+module.exports = app;
---
+++ controllers/transactionController.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const allTransactions = require("../models/transactions");
+const transactions = express.Router();
+
+transactions.get("/", (req, res) => {
+    res.json(allTransactions);
+});
+
+transactions.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    if(allTransactions[id]){
+        res.json(allTransactions[id]);
+    } else {
+        res.status(404).json({error: "Transaction not found"});
+    }
+});
+
+transactions.post("/", (req, res) => {
+    allTransactions.push(req.body);
+    res.json(allTransactions[allTransactions.length-1]);
+});
+
+transactions.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    
+    if(allTransactions[id]){
+        let deletedTransaction = allTransactions.splice(id, 1);
+        res.json(deletedTransaction)
+    } else {
+        res.status(404).json({error: "Transaction not found"})
+    }
+});
+
+transactions.put("/:id", (req, res) => {
+    const { id } = req.params;
+
+    if(!allTransactions[id]){
+        res.status(422).json({
+            error: "Transaction not found"
+        });
+        return;
+    }
+
+    let { name, amount, date, from } = req.body;
+    if(name && (Number.isFinite(amount)) && date && from){
+        allTransactions[id] = {
+            name, amount, date, from
+        };
+        res.json(allTransactions[id]);
+    } else {
+        res.status(422).json({
+            error: "Please provide all fields"
+        });
+    }
+});
+
+module.exports = transactions;
