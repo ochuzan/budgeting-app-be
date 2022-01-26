e0790b37415c2c89d3d785c842be736480205a0b
Oliver Chuzan <ochuzan@gmail.com>
2022-01-25 20:31:44 -0500

    Added categories to data

--- models/transactions.js
+++ models/transactions.js
@@ -3,42 +3,49 @@ module.exports = [
       date: "2021-12-03",
       name: "Groceries",
       amount: -59.21,
-      from: "Stop & Shop"
+      from: "Stop & Shop",
+      category: "Food"
     },
     {
       date: "2021-12-04",
-      name: "Food",
+      name: "Taco Bell",
       amount: -30.47,
-      from: "Uber Eats"
+      from: "Uber Eats",
+      category: "Food"
     },
     {
       date: "2021-12-10",
-      name: "Income",
+      name: "Paycheck",
       amount: 2000,
-      from: "Etsy"
+      from: "Etsy",
+      category: "Income"
     },
     {
       date: "2021-12-14",
       name: "Metrocard",
       amount: -2.75,
-      from: "MTA"
+      from: "MTA",
+      category: "Transportation"
     },
     {
       date: "2021-12-23",
       name: "Music Subscription",
       amount: -9.99,
-      from: "Spotify"
+      from: "Spotify",
+      category: "Subscriptions"
     },
     {
       date: "2021-12-24",
-      name: "Income",
+      name: "Paycheck",
       amount: 2000,
-      from: "Etsy"
+      from: "Etsy",
+      category: "Income"
     },
     {
       date: "2021-12-28",
       name: "Toll",
       amount: -60,
-      from: "Ez-pass"
+      from: "Ez-pass",
+      category: "Transportation"
     },
   ];
